fe3d870025914cfffdfed6971fcdec81e8c9b423
RxGoodness <adedokuntoyyib@gmail.com>
2024-02-14 23:32:50 +0100

    Refresh token

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,23 +2,15 @@ import { Router } from "express";
 import {
   updateUser,
   deleteUser,
-  getOneUser,
   getUserById,
-  adminUpdateUser
-  } from "../controllers/user.controller";
-
+  adminUpdateUser,
+} from "../controllers/user.controller";
 import bodyValidator from "../utils/bodyValidator";
 import { auth, isAdmin, userValidationRules } from "../middlewares";
 
 const router = Router();
 
-router.put(
-  "/",
-  auth,
-  userValidationRules(),
-  bodyValidator,
-  updateUser
-);
+router.put("/", auth, userValidationRules(), bodyValidator, updateUser);
 
 router.patch(
   "/:user_id",
@@ -32,7 +24,5 @@ router.patch(
 router.delete("/:user_id", auth, isAdmin, deleteUser);
 
 router.get("/", auth, getUserById);
-
-router.get("/services/:user_id", getOneUser);
 
 export default router;
